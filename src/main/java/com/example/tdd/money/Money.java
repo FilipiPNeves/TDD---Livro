e273fdfcd5e23eb208ced286b0e4ba689c2529db
FilipiPNeves <filipi.pereira.neves@gmail.comgit config --global user.name Filipi>
2025-09-24 20:11:40 -0300

    Finished Chapter 12 of TDD book

--- src/main/java/com/example/tdd/money/Money.java
+++ src/main/java/com/example/tdd/money/Money.java
@@ -28,6 +28,9 @@ public class Money implements Expression {
         return amount + " " + currency;
     }
     Expression plus(Money addend) {
-        return new Money(amount + addend.amount, currency);
+        return new Sum(this, addend);
+    }
+    public Money reduce(String to) {
+        return this;
     }
 }
