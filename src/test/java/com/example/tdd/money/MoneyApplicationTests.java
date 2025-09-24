df469e3f228631977d77a52d564f50f82e8d8c32
FilipiPNeves <filipi.pereira.neves@gmail.comgit config --global user.name Filipi>
2025-09-24 15:35:35 -0300

    Finished Chapter 11 of TDD book

--- src/test/java/com/example/tdd/money/MoneyApplicationTests.java
+++ src/test/java/com/example/tdd/money/MoneyApplicationTests.java
@@ -16,13 +16,6 @@ class MoneyApplicationTests {
 		assertEquals(Money.dollar(10), five.times(2));
 		assertEquals(Money.dollar(15), five.times(3));
 	}
-
-	@Test	
-	public void testFrancMultiplication() {
-		Money five = Money.franc(5);
- 		assertEquals(Money.franc(10), five.times(2));
- 		assertEquals(Money.franc(15), five.times(3));
-	}
 	
 	@Test
 	public void testEquality() {
